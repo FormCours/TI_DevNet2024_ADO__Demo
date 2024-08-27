1d126a24a1479a7b0bf5f98e29bd3ec8ef33456e
Aurélien <aurelien.strimelle@bstorm.be>
2024-08-27 14:58:35 +0200

    Mise en place d'un projet pour consommer la DB 😎

---
+++ Demo_ADO.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+// Avant de commencer → Installer le bibliotheque ADO pour le type de server ciblé !
+// Package NuGet : Microsoft.Data.SqlClient
+
+
+const string connectionString = @"Data Source=DESKTOP-QLR948L;Initial Catalog=DemoADO_Book;Trusted_Connection=True;Trust Server Certificate=True";
+
+
+SqlConnection connection = new SqlConnection(connectionString);
+Console.WriteLine($"Connexion à la DB : {connection.State}");
+
+// Ouverture de la connexion vers la DB
+connection.Open();
+Console.WriteLine($"Connexion à la DB : {connection.State}");
+
+
+// Fermeture de la connexion vers la DB
+connection.Close();
+Console.WriteLine($"Connexion à la DB : {connection.State}");
+
